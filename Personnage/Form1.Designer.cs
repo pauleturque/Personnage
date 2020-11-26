2cf3c3d134a1fbf1615f489b1f4af7cc2f1a2ef5
pauleturque <paulineleturque8@gmail.com>
2020-11-26 11:16:31 +0100

    Ajoutez des fichiers projet.

---
+++ Personnage/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Personnage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreer
+            // 
+            this.btnCreer.Location = new System.Drawing.Point(56, 44);
+            this.btnCreer.Name = "btnCreer";
+            this.btnCreer.Size = new System.Drawing.Size(233, 67);
+            this.btnCreer.TabIndex = 0;
+            this.btnCreer.Text = "Créer personnage";
+            this.btnCreer.UseVisualStyleBackColor = true;
+            this.btnCreer.Click += new System.EventHandler(this.btnCreer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 676);
+            this.Controls.Add(this.btnCreer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreer;
+    }
+}
+
